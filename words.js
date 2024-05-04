8602c5ad39c97953870a8b9297d4995cf44d073e
joliecannoli <keoanj12@gmail.com>
2024-05-04 13:10:36 -1000

    Touch ups

--- words.js
+++ words.js
@@ -3,4 +3,40 @@ const wordList = [
         word: "ardently",
         hint: "Mr. Darcy's first confession to Elizabeth in the 2005 film adaptation."
     },
+    {
+        word: "countenance",
+        hint: "Appearance conveyed by one's face, Mr. Bingley had a pleasant"
+    },
+    {
+        word: "governess",
+        hint: "A woman employed as a private tutor that teaches a child in their home."
+    },
+    {
+        word: "amiable",
+        hint: "Generally agreeable"
+    },
+    {
+        word: "chambermaid",
+        hint: "A woman whose occupation is taking care of bedrooms."
+    },
+    {
+        word: "commission",
+        hint: "Official certicate that confers rank"
+    },
+    {
+        word: "avarice",
+        hint: "A great desire to have wealth."
+    },
+    {
+        word: "embargo",
+        hint: "Any form of restriction/restraint."
+    },
+    {
+        word: "heinous",
+        hint: "Wicked, abominable."
+    },
+    {
+        word: "incessantly",
+        hint: "To be never ceasing."
+    },
 ];
